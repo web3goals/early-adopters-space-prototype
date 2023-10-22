dc035d2ce8310b9396c3bba28e93e637c8db3655
kiv1n <aloha@kiv1n.ru>
2023-10-22 16:16:45 +0300

    Update script to deploy project contract

--- contracts/scripts/deploy/project.ts
+++ contracts/scripts/deploy/project.ts
@@ -1,9 +1,24 @@
 import { ethers } from "hardhat";
+import hre from "hardhat";
+import { DeployContractOptions } from "@nomicfoundation/hardhat-ethers/types";
 
 async function main() {
   console.log("👟 Start to deploy project contract");
   // Deploy project contract
-  const contract = await ethers.deployContract("Project", [10]);
+  let options: DeployContractOptions | undefined;
+  if (hre.network.name === "filecoinCalibration") {
+    options = {
+      maxFeePerGas: 100,
+      maxPriorityFeePerGas: 100,
+      gasLimit: 10_000_000,
+    };
+  }
+  if (hre.network.name === "mantleTestnet") {
+    options = {
+      gasLimit: 10_000_000,
+    };
+  }
+  const contract = await ethers.deployContract("Project", [20], options);
   await contract.waitForDeployment();
   console.log(`✅ Project contract deployed to ${contract.target}`);
 }
